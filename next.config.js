5ceda0469f02c57c60def96313202e9e3bbdc12f
Eugene <87051223+Swordaac@users.noreply.github.com>
2025-09-28 12:28:56 -0400

    rewrite to next config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000', 'flower-store-gt.onrender.com'],
     },
   },
+  async rewrites() {
+    // Mirror rewrites to ensure /api/* proxies to the Express backend started by server.js
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5001/api/:path*',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
